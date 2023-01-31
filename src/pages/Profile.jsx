08a468e04fa5c26b5a68dffd32f3d851faaeed98
Wlauber <wlaubersamuel@gmail.com>
2023-01-31 11:13:01 -0300

    requisito 13

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,11 +1,40 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
+import { getUser } from '../services/userAPI';
+import Carregando from './Carregando';
 
 class Profile extends React.Component {
+  state = {
+    usuario: {},
+    loading: false,
+  };
+
+  componentDidMount() {
+    this.setState({
+      loading: true,
+    }, async () => {
+      const usuario = await getUser();
+      this.setState({ usuario, loading: false });
+    });
+  }
+
   render() {
+    const { usuario, loading } = this.state;
     return (
       <div data-testid="page-profile">
         <Header />
+        { loading ? (
+          <Carregando />
+        ) : (
+          <div>
+            <img src={ usuario.image } alt="" data-testid="profile-image" />
+            <h2>{ usuario.name }</h2>
+            <p>{ usuario.email }</p>
+            <p>{ usuario.description }</p>
+            <Link to="/profile/edit">Editar perfil</Link>
+          </div>
+        ) }
       </div>
     );
   }
